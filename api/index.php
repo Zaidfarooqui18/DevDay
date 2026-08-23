ed84a8280d2394ea9c9436d2ba7a1e8746bc87fb
Zaidfarooqui18 <zfarooqui356@gmail.com>
2026-08-23 23:31:36 +0530

    Fix PSR-4 class casing and add universal autoloader for Linux Vercel runtime

--- api/index.php
+++ api/index.php
@@ -1,6 +1,33 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+// Universal autoloader fallback for Linux case-sensitive environments
+spl_autoload_register(function ($class) {
+    if (!str_starts_with($class, 'DevDay\\')) {
+        return;
+    }
+    $relativeClass = substr($class, 7);
+    $parts = explode('\\', $relativeClass);
+    $className = array_pop($parts);
+    $dir = strtolower(implode('/', $parts));
+    $baseDir = dirname(__DIR__);
+
+    $candidates = [
+        $baseDir . '/' . ($dir ? $dir . '/' : '') . $className . '.php',
+        $baseDir . '/' . ($dir ? $dir . '/' : '') . strtolower($className) . '.php',
+        $baseDir . '/' . str_replace('\\', '/', $relativeClass) . '.php',
+    ];
+
+    foreach ($candidates as $file) {
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
+
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require_once dirname(__DIR__) . '/vendor/autoload.php';
+}
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
